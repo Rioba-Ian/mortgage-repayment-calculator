4366172c1ada4db2638b27d67eafaf47a02f01c3
RiobaIan <ian.rioba@globallinkplus.com>
2024-07-03 12:06:33 +0300

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,21 @@
+import { Title } from "@solidjs/meta";
+
+export default function Home() {
+ return (
+  <main class="contain-layout">
+   <h1>Hello Mum.</h1>
+   Mortgage Calculator Clear All Mortgage Amount Mortgage Term Interest Rate
+   Mortgage Type Repayment Interest Only Calculate Repayments Results shown here
+   Complete the form and click “calculate repayments” to see what your monthly
+   repayments would be. Your results Your results are shown below based on the
+   information you provided. To adjust the results, edit the form and click
+   “calculate repayments” again. Your monthly repayments Total you'll repay over
+   the term
+   <div class="attribution">
+    Challenge by{" "}
+    <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+    Coded by <a href="#">Your Name Here</a>.
+   </div>
+  </main>
+ );
+}
